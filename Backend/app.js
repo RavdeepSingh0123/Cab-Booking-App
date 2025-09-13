6903bb17adf2dd153bc180e45534f245087256e4
RavdeepSingh0123 <107946867+RavdeepSingh0123@users.noreply.github.com>
2025-09-14 01:53:32 +0530

    User Registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,18 @@
 const dotenv=require('dotenv')
 dotenv.config();
 const express=require('express');
-const cors=require('cors')
-
+const cors=require('cors');
+const connectToDb = require('./DB/db');
+const userRoutes=require('./routes/user.routes')
 const app=express();
+connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.send("Hello");
 });
 
+app.use('/users',userRoutes)
 module.exports=app;
