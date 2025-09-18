94e01491b26b00b53159093b3a2835ad3fb13303
RavdeepSingh0123 <107946867+RavdeepSingh0123@users.noreply.github.com>
2025-09-18 23:38:30 +0530

    Added Login,Profile and logout routes for captain

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const {body} = require('express-validator');
 const router=express.Router();
-const captainController=require('../Controllers/captain.controller')
+const captainController=require('../Controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -12,5 +13,15 @@ router.post('/register',[
     body('vehicle.capacity').isLength({min:1}).withMessage('Vehicle Capacity must be atleast 1 pessanger'),
     body('vehicle.vehicleType').isIn(['car','motorcycle','auto']).withMessage('Invalid Vehicle type')
 ],captainController.registerCaptain)
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long')
+],captainController.loginCaptain)
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile)
+
+router.post('/logout',authMiddleware.authCaptain,captainController.logoutCaptain)
+
 
 module.exports=router;
